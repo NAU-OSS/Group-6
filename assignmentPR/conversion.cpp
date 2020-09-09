605ca6d608e36734b04bcb5d929311a98a2daa76
ziang-zhou <68038157+ziang-zhou@users.noreply.github.com>
2020-09-08 18:20:19 -0700

    Update conversion.cpp

--- assignmentPR/conversion.cpp
+++ assignmentPR/conversion.cpp
@@ -1,12 +1,16 @@
 #include<stdio.h>
+#include<ctype.h>
 
-//Author:Group6_Shangyi
-//Edit date: Sep 2,2020
+#define TRUE 1
+#define FALSE 0
+//Author:Group6_Shangyi Group6_Ziang
+//Edit date: Sep 8,2020
 //Declare function
+int isRealNumber(char numString[]);
 char* characteristic(char numString[],int index);
 char* mantissa(char numString[],int index);
 
-//create global array,I know  Global variable is bad, but I am not familiar with stack in C 
+//create global array,I know  Global variable is bad, but I am not familiar with stack in C
 char number[] = "123.456";
 char character[sizeof(number)];
 char numerator[sizeof(number)];
@@ -14,67 +18,78 @@ char numerator[sizeof(number)];
 //The main function
 int main()
 {
-	//The pointers and array for print
-	char* character;
-	char* numerator;
-	char denominator[sizeof(number)] = "1";
-	
-	//Declare the indexes used in the main function
-	int index,midindex = 0,denIndex = 1;
-	
-	//I use for loop to find the position of '.' in the string
-	for(index = 0; index < sizeof(number); index++)
-	{
-		if(number[index] == '.')
-		{
-			midindex = index;
-		}
-	}
+    //jusge if the string is real number
+    if(isRealNumber(number)){
+        printf("The string is not real number. Terminate the program.\n");
+        exit(0);
+    }
+    else{
+      printf("The string is real number. The program continues.\n");
+    }
+    
+    //The pointers and array for print
+    char* character;
+    char* numerator;
+    char denominator[sizeof(number)] = "1";
+    
+    //Declare the indexes used in the main function
+    int index,midindex = 0,denIndex = 1;
+    
+    //I use for loop to find the position of '.' in the string
+    for(index = 0; index < sizeof(number); index++)
+    {
+        if(number[index] == '.')
+        {
+            midindex = index;
+        }
+    }
 
-	//The temp is the magnitude of denominator
-	//Why -2 ? That's because the length of '.' and '\0'
-	int temp = sizeof(number) - midindex - 2;
-	while(temp >0)
-	{
-		denominator[denIndex] = '0';
-		denIndex++;
-		temp--;
-	}
+    //The temp is the magnitude of denominator
+    //Why -2 ? That's because the length of '.' and '\0'
+    int temp = sizeof(number) - midindex - 2;
+    while(temp > 0)
+    {
+        denominator[denIndex] = '0';
+        denIndex++;
+        temp--;
+    }
 
-	//The judgment and the print action
-	if(characteristic(number,midindex)&&mantissa(number,midindex))
-	{
-		character = characteristic(number,midindex);
-		
-		numerator = mantissa(number,midindex);
-		
-		printf("The characteristic for the number %s is %s and the mantissa is %s over %s.\n",
-		number,character,numerator,denominator);
-	}else
-	{
-		printf("Error,Please check input\n");
-	}
-	return 0;
- } 
+    //Print what is in the sting
+        character = characteristic(number,midindex);
+        numerator = mantissa(number,midindex);
+        printf("The characteristic for the number %s is %s and the mantissa is %s over %s.\n",
+        number,character,numerator,denominator);
+    return 0;
+ }
 
+int isRealNumber(char numString[]){
+    int index = 0;
+    while(numString[index++]){
+      if(!isdigit(numString[index-1]) && numString[index-1]!='.' && numString[0]!='-'){
+      return TRUE;
+      }
+    }
+    return FALSE;
+}
 
 //Break the string(character array) into the characteristic
 char* characteristic(char numString[],int index){
-	int charaIndex = 0;
-	for(charaIndex; charaIndex < index; charaIndex++)
-	{
-		character[charaIndex] = numString[charaIndex];
-	}
-	return character;
+    int charaIndex = 0;
+    for(charaIndex = 0; charaIndex < index; charaIndex++)
+    {
+        character[charaIndex] = numString[charaIndex];
+    }
+    return character;
 }
 
 //Break the string(character array) into the mantissa
 char* mantissa(char numString[],int index){
-	int numeratorIndex = 0;
-	for(index; index < sizeof(number); index++)
-	{
-		numerator[numeratorIndex] = numString[index+1];
-		numeratorIndex ++;
-	}
-	return numerator;
+    int numeratorIndex = 0;
+    for(index; index < sizeof(number); index++)
+    {
+        numerator[numeratorIndex] = numString[index+1];
+        numeratorIndex ++;
+    }
+    return numerator;
 }
+
